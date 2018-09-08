91a366f68f3d45dd9c080e887021f242d537362d
unknown <nathan.pabst@gmail.com>
2018-09-08 13:38:58 -0500

    20 numbers generated and printing

---
+++ RandomSquared/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RandomSquared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using the Random class, generate a list of 20 random numbers that are in the range of 1-50.
+            Console.WriteLine("20 random integers between 1 and 50");
+            //create a list
+            var Random = new Random();
+            for (int ctr = 1; ctr <= 20; ctr++)
+                Console.WriteLine(Random.Next(1, 51));
+
+            //populate the list
+            Console.ReadLine();
+        }
+    }
+}
